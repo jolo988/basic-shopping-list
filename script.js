91c885809755a3571ca029fed4767730e0f1a434
Jonathan Lo <126200054+jolo988@users.noreply.github.com>
2023-03-13 13:33:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
+// Select elements needed to create shopping list, and cache queries. 
+
 var button = document.getElementById('enter');
 var input = document.getElementById('userinput');
 var ul = document.querySelector("ul");
 
-// SELECT ELEMENTS WE NEED, cache queries. 
-// Quicker to get element by ID vs class
-// reference to button "enter"
-// user input of what's typed into placeholder
-// assign query select ul to function
-
-
+// measure length of input (input requiring length of > 0, as per if statement below)
 function inputLength() {
 	return input.value.length;
 }
 
-
+// append input to list (under parent list 'ul')
 function createListElement() {
 	var li = document.createElement("li"); 
 	li.appendChild(document.createTextNode(input.value));
@@ -21,36 +17,26 @@ function createListElement() {
 	input.value = "";
 }
 
-// function -> whenever user clicks, run the function below:
-// (listen to the click event -> and perform action/function)
-// Adding elements to the bottom of the list:
-// Assign li = Create element
-// append input.value (whatever is typed) as list -> by creating a new NODE
-// append list to UL parent 
-
-
+// input appends to list only if characters/input > 0
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
-// wrap in if statement - if nothing is typed -> won't run funct
-// input value will be empty after each value enter
 
+// input appends to list only if characters/input > 0 AND presses 'enter' on keyboard
+// NOTE: can use event.which === 13
+// https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
 function addListAfterEnter(event) {
 	if (inputLength() > 0 && event.code === "Enter") {
 		createListElement();
 	};
 }
 
-// event parameter needs to be received to pull information
-// can use event.which === 13
-// https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
-// if input AND there's keyboard enter -> run function 
-
-
+// run addListAfterClick function when user clicks button
 button.addEventListener("click", addListAfterClick);
 
+// run addListAfterEnter function when user presses 'enter' on keyboard
 input.addEventListener("keypress", addListAfterEnter);
 
 
